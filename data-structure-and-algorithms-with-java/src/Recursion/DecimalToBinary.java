8cab8b492b2fb580dd5cf01b17a338902c00cc9f
XIVIXMMI <lethaikhoinguyen@gmail.com>
2024-02-21 00:05:12 +0700

    Recursive

---
+++ data-structure-and-algorithms-with-java/src/Recursion/DecimalToBinary.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+
+public class DecimalToBinary {
+    public static void main(String[] args) {
+
+        System.out.println(findBinary(100," "));
+
+    }
+    public static String findBinary(int decimal, String result){
+        if(decimal == 0){
+            return result;
+        }
+        result = decimal % 2 + result;
+        return findBinary(decimal / 2, result);
+    }
+}
