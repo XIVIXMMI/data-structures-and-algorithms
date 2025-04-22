35c282aab35fe400a789ef7e47a218436cdc3baf
XIVIXMMI <lethaikhoinguyen@gmail.com>
2025-04-22 16:53:16 +0700

    top interview 1-redesign solution

--- leetcode/top-interview-150/88/Solution.java
+++ leetcode/top-interview-150/88/Solution.java
@@ -14,21 +14,32 @@
  */
 
 public class Solution {
-  public void merge(int[] nums1, int m, int[] nums2, int n) {
+  public static void merge(int[] nums1, int m, int[] nums2, int n) {
     int i = m - 1; // pointer for nums1
     int j = n - 1; // pointer for nums2
     int k = m + n - 1; // fill position from the end
 
-    while (i >= 0 && j >= 0) {
-      if (nums1[i] > nums2[j]) {
-        nums1[k--] = nums1[i--];
-      } else {
-        nums1[k--] = nums2[j--];
-      }
-    }
-    // copy remaining elements from nums2 if any
     while (j >= 0) {
-      nums1[k--] = nums2[j--];
+      if (i > 0 && nums1[i] >= nums2[j]) {
+        nums1[k] = nums1[i];
+        i--;
+      } else {
+        nums1[k] = nums2[j];
+        j--;
+      }
+      k--;
     }
+
+    for (int num : nums1) {
+      System.out.println(num);
+    }
+  }
+
+  public static void main(String[] args) {
+    int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+    int[] nums2 = { 2, 5, 6 };
+    int m = 3;
+    int n = 3;
+    merge(nums1, m, nums2, n);
   }
 }
