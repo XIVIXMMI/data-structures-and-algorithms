ac37e75e04f2e0f65540a500b47f587fef69d2d2
XIVIXMMI <lethaikhoinguyen@gmail.com>
2025-05-15 21:21:21 +0700

    top interview 12

---
+++ leetcode/top-interview-150/380/RandomizedSet.java
@@ -0,0 +1,45 @@
+/**
+ *
+ * INSERT DELETE GETRANDOM O(1)
+ *
+ * Implement the RandomizedSet class:
+ * - `RandomizeSet()` initializes the RandomizeSet object
+ * - `bool insert(int val)` inserts an item `val` into the set if not present
+ * Returns `true` if the item was not present, `false` otherwise
+ * - `bool remove(int val)` Removes an item `val` from the set if present
+ * Return `true` if the item was present, `false` otherwise
+ * - `int getRandom()` Returns a random element from the current set of elements
+ * (it's guaranteed that at least one element exist when this method is called)
+ * Each element must have the *same probability* of being Returns
+ *
+ * You must implement the functions of the class such that each function works
+ * in *average* `O(1)` time complexity
+ *
+ */
+
+class RandomizedSet {
+
+  public RandomizedSet() {
+
+  }
+
+  public boolean insert(int val) {
+
+  }
+
+  public boolean remove(int val) {
+
+  }
+
+  public int getRandom() {
+
+  }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
