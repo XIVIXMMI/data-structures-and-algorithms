282298632b2cd92b0f5f5502c1cbfce75743494b
XIVIXMMI <lethaikhoinguyen@gmail.com>
2025-06-15 23:39:00 +0700

    top interview day 14

---
+++ leetcode/top-interview-150/383/Solution.java
@@ -0,0 +1,14 @@
+/**
+ * Given two strings ransomNote and magazine,
+ * return true if ransomNote can be constructed by using the letters from
+ * magazine and false otherwise
+ * Each letter in magazine can only be used once in ransomNote.
+ */
+
+public class Solution {
+
+  public boolean canConstruct(String ramsomNote, String magazine) {
+
+    return false;
+  }
+}
