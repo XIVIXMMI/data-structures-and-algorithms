b31f8893d6fa637886b3efebca4bb1ae815e9c53
XIVIXMMI <lethaikhoinguyen@gmail.com>
2025-06-18 11:09:55 +0700

    top interview day 14

--- leetcode/top-interview-150/383/Solution.java
+++ leetcode/top-interview-150/383/Solution.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * Given two strings ransomNote and magazine,
  * return true if ransomNote can be constructed by using the letters from
@@ -7,8 +9,37 @@
 
 public class Solution {
 
-  public boolean canConstruct(String ramsomNote, String magazine) {
+  public static boolean canConstruct(String ransomNote, String magazine) {
 
-    return false;
+    HashMap<Character, Integer> map = new HashMap<>();
+
+    char[] character = ransomNote.toCharArray();
+    char[] magazineChar = magazine.toCharArray();
+
+    for (char c : character) {
+      if (map.containsKey(c)) {
+        map.put(c, map.get(c) + 1);
+      } else {
+        map.put(c, 1);
+      }
+
+    }
+
+    for (char c : magazineChar) {
+      if (map.containsKey(c)) {
+        map.put(c, map.get(c) - 1);
+        if (map.get(c) == 0) {
+          map.remove(c);
+        }
+      }
+    }
+
+    return map.isEmpty();
+  }
+
+  public static void main(String[] args) {
+    String ransomNote = "aa";
+    String magazine = "aab";
+    System.out.println(canConstruct(ransomNote, magazine));
   }
 }
