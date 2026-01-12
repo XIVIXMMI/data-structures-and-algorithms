a61fbca737054316479683e5d23244b49dff4611
XIVIXMMI <lethaikhoinguyen@gmail.com>
2026-01-12 16:35:18 +0700

    top-interview-150: 217 Contains Duplicate

---
+++ leetcode/top-interview-150/217/Solution.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * CONTAINS DUPLICATE
+ * Given an array num, return true if any value appears at least twice in the
+ * array
+ * and return false if every element is distinct.
+ */
+
+public class Solution {
+
+    public static boolean containsDuplicate(int[] nums) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int count : map.values()) {
+            if (count > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3 }; // false
+        System.out.println(containsDuplicate(nums));
+    }
+}
