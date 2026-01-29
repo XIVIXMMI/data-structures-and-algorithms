4f62e13d41a2eb4993a40790d8d87749701fea23
XIVIXMMI <lethaikhoinguyen@gmail.com>
2026-01-29 21:46:48 +0700

    top-interview-150: 290 word pattern

--- leetcode/top-interview-150/290/Solution.java
+++ leetcode/top-interview-150/290/Solution.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * WORD PATTERN
@@ -12,7 +15,26 @@
 
 class Solution {
     public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (pattern.length() != words.length)
+            return false;
 
+        Map<Character, String> p2w = new HashMap<>();
+        Map<String, Character> w2p = new HashMap<>();
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String w = words[i];
+            if (p2w.containsKey(c) && !p2w.get(c).equals(w)) {
+                return false;
+            } else {
+                p2w.put(c, w);
+            }
+            if (w2p.containsKey(w) && w2p.get(w) != c) {
+                return false;
+            } else {
+                w2p.put(w, c);
+            }
+        }
         return true;
     }
 }
