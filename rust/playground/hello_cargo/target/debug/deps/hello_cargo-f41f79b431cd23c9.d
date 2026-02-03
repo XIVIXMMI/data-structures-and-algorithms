ca4079d22a94ed76d0fd8ecf3ded48cc85411b98
XIVIXMMI <lethaikhoinguyen@gmail.com>
2026-02-03 12:47:29 +0700

    getting start with rust

---
+++ rust/playground/hello_cargo/target/debug/deps/hello_cargo-f41f79b431cd23c9.d
@@ -0,0 +1,5 @@
+/Users/omori/code/DSA/rust/playground/hello_cargo/target/debug/deps/hello_cargo-f41f79b431cd23c9.d: src/main.rs
+
+/Users/omori/code/DSA/rust/playground/hello_cargo/target/debug/deps/libhello_cargo-f41f79b431cd23c9.rmeta: src/main.rs
+
+src/main.rs:
---
+++ rust/playground/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/omori/code/DSA/rust/playground/hello_cargo/target/debug/hello_cargo: /Users/omori/code/DSA/rust/playground/hello_cargo/src/main.rs
