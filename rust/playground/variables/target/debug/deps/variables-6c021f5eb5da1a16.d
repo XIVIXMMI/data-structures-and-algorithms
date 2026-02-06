43000db011b5536f3c8c63dacdf5c7c0c99e83f8
XIVIXMMI <lethaikhoinguyen@gmail.com>
2026-02-06 12:44:00 +0700

    variables and immutable

---
+++ rust/playground/variables/target/debug/deps/variables-6c021f5eb5da1a16.d
@@ -0,0 +1,5 @@
+/Users/omori/code/DSA/rust/playground/variables/target/debug/deps/variables-6c021f5eb5da1a16.d: src/main.rs
+
+/Users/omori/code/DSA/rust/playground/variables/target/debug/deps/variables-6c021f5eb5da1a16: src/main.rs
+
+src/main.rs:
---
+++ rust/playground/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/omori/code/DSA/rust/playground/variables/target/debug/variables: /Users/omori/code/DSA/rust/playground/variables/src/main.rs
