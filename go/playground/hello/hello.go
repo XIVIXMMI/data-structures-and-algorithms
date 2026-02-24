eb7e8036492783bc0f75b06fe9662b2a3c9fcfcd
XIVIXMMI <lethaikhoinguyen@gmail.com>
2026-02-24 16:33:22 +0700

    create module and call from another module

--- go/playground/hello/hello.go
+++ go/playground/hello/hello.go
@@ -1,10 +1,11 @@
 package main
 
-import "fmt"
-
-import "rsc.io/quote"
+import (
+	"fmt"
+	"example.com/greetings"
+)
 
 func main() {
-	// fmt.Println("Hello World")
-	fmt.Println(quote.Go())
+	message := greetings.Hello("Bill")
+	fmt.Println(message)
 }
