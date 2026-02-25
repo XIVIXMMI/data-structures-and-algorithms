fcd3e59453fb82bd232f27427c6413902e9f1f5a
XIVIXMMI <lethaikhoinguyen@gmail.com>
2026-02-25 21:22:15 +0700

    return a random greeting

--- go/playground/greetings/greetings.go
+++ go/playground/greetings/greetings.go
@@ -1,14 +1,24 @@
 package greetings
 
 import (
-	"fmt"
 	"errors"
+	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message, nil 
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v ! Well met!",
+	}
+	return formats[rand.Intn(len(formats))]
 }
--- go/playground/hello/hello.go
+++ go/playground/hello/hello.go
@@ -11,8 +11,10 @@ func main() {
 	log.SetPrefix("greeting: ")
 	log.SetFlags(0)
 
-	// Request a greeeting message 
-	message, err := greetings.Hello("")
+	// Request a greeeting message
+	message, err := greetings.Hello("Bill")
+	// If an error was return, print it to console
+	// and exit the program!
 	if err != nil {
 		log.Fatal(err)
 	}
