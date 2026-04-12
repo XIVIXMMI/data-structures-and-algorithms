a47f35b8bad4cd9267a681718f118b8cd5359718
XIVIXMMI <lethaikhoinguyen@gmail.com>
2026-04-12 21:55:16 +0700

    top-interview-150: 49 Group Anagrams

---
+++ leetcode/top-interview-150/49/Java/Solution.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        String[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
+        List<List<String>> result = groupAnagrams(strs);
+        System.out.println(result);
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String string : strs) {
+            char[] chars = string.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            List<String> list = map.computeIfAbsent(key, k -> new ArrayList<>());
+            list.add(string);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
