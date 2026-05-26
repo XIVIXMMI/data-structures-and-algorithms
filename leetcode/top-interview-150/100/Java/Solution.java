fae66217394944069bbc05b31931000884874429
Nguyen Le <lethaikhoinguyen@gmail.com>
2026-05-26 14:40:49 +0700

    top-interview-150: 100 Same Tree

--- leetcode/top-interview-150/100/Java/Solution.java
+++ leetcode/top-interview-150/100/Java/Solution.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) {
+        if(q == null && p == null ) {
             return true;
         }
-        if ((p == null && q != null) || (p != null && q == null)) {
+        if(( q == null && p != null) || ( q != null && p == null )) {
             return false;
         }
-        if (p.val != q.val)
-            return false;
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        if( q.val != p.val ) return false;
+        return isSameTree(p.left,q.left) && isSameTree(p.right, q.right);
     }
 
     public class TreeNode {
