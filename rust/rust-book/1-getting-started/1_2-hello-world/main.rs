2437fb76a18250b6a580b01492e425e6c813da83
Nguyen Le <lethaikhoinguyen@gmail.com>
2026-06-28 11:50:09 +0700

    rust-book: chapter 1

---
+++ rust/rust-book/1-getting-started/1_2-hello-world/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello World"); 
+}
+
+// " ! " -> is call a Rust macro if not have " ! " -> this mean calling function 
+// Macro mean "code that generates code", just use ! and the Macro will expand to a lot line of code before
+
+// " ; " -> indicate that this expression is over!
+
+
+
