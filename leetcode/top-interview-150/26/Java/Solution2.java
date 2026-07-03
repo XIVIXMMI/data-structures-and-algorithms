b9baf1612a98359ea24a5e73317d5b38e93126ba
Nguyen Le <lethaikhoinguyen@gmail.com>
2026-07-03 09:59:34 +0700

    26. Remove Duplicates from Sorted Array

---
+++ leetcode/top-interview-150/26/Java/Solution2.java
@@ -0,0 +1,15 @@
+class Solution2 {
+  public int removeDuplicates(int[] nums) {
+    int i = 1;
+    int j = 0;
+
+    while( i < nums.length){
+      if( nums[i] > nums[j]){
+        j++;
+        nums[j] = nums[i];
+      }
+      i++;
+    }
+    return j + 1;
+  }
+}
