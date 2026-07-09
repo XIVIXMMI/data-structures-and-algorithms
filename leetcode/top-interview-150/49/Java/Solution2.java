e986fd386d140c85f06912e4c6330d16d1d85d0c
Nguyen Le <lethaikhoinguyen@gmail.com>
2026-07-09 11:30:16 +0700

    49. Group Anagrams

--- leetcode/top-interview-150/49/Java/Solution2.java
+++ leetcode/top-interview-150/49/Java/Solution2.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -18,6 +19,17 @@ public class Solution2 {
             map.computeIfAbsent(key.toString(), k -> new ArrayList<>()).add(s);
         }
 
+        return new ArrayList<>(map.values());
+    }
+
+    public List<List<String>> groupAnagramsOptimize(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
         return new ArrayList<>(map.values());
     }
 
