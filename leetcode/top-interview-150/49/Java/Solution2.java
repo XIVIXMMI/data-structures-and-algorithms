48badf7062232dbcafbe305abbea02125fc00588
Nguyen Le <lethaikhoinguyen@gmail.com>
2026-07-09 10:59:15 +0700

    49. Group Anagram

--- leetcode/top-interview-150/49/Java/Solution2.java
+++ leetcode/top-interview-150/49/Java/Solution2.java
@@ -1,8 +1,30 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Solution2 {
     public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            int[] count = new int[26];
+            StringBuilder key = new StringBuilder();
+            for (int i = 0; i < s.length(); i++) {
+                count[s.charAt(i) - 'a']++;
+            }
+            for (int c : count) {
+                key.append(c + "#");
+            }
+            map.computeIfAbsent(key.toString(), k -> new ArrayList<>()).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
 
     }
 
 }
+
+// strs = ["eat","tea","tan","ate","nat","bat"]
