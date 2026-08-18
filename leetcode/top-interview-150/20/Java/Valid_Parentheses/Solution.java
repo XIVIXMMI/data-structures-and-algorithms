3dc8798922a2956fc36c9f38d256eea0c9a8912e
Nguyen Le <lethaikhoinguyen@gmail.com>
2026-08-18 09:24:42 +0700

    20. Valid Parentheses

--- leetcode/top-interview-150/20/Java/Valid_Parentheses/Solution.java
+++ leetcode/top-interview-150/20/Java/Valid_Parentheses/Solution.java
@@ -4,23 +4,29 @@ import java.util.HashMap;
 
 class Solution {
     public boolean isValid(String s) {
+        // use a stack to make sure the order is correct
         Deque<Character> stack = new ArrayDeque<>();
-        HashMap<Character,Character> map = new HashMap<>();
-        map.put(')', '(');
-        map.put('}', '{');
-        map.put(']', '[');
+        // using hashmap to store the corresponding pairs of characters
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put(')','(');
+        map.put('}','{');
+        map.put(']','[');
 
+        // loop through the string
         for( int i = 0; i < s.length(); i++) {
-
-            if( s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') 
+            // if the character is one of '( { [' 
+            // -> then push them to stack
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') 
                 stack.push(s.charAt(i));
-
-            if( s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']') {
-                if (stack.peek() == null ) return false;
-                if (!stack.pop().equals(map.get(s.charAt(i)))) return false;
+            // if the character is one of ') } ]' 
+            // -> find the value in the maeep then return false if it doesn't equal otherwise pop the top of the stack out
+            if(s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']') {
+                if(stack.peek() == null ) return false;
+                if(!stack.pop().equals(map.get(s.charAt(i)))) return false;
             }
         }
 
+        // after loop throught the string, if the stack still contains characters then return false otherwise return true
         return stack.peek() == null ? true : false;
     }
 }
